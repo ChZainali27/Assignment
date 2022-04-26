976b49fc1b29689cd10b9f11e540d30ead5d5fb9
ChZainali27 <104452690+ChZainali27@users.noreply.github.com>
2022-04-26 22:01:14 +0500

    Add files via upload

---
+++ AllLiterals.java
@@ -0,0 +1,15 @@
+public class AllLiterals
+{
+	public static void main (String arg[])
+	{
+		int a=20;
+		long b=38L;
+		float c=3.13f;
+		float d=3.45e5f;
+		double e=7.4;
+		char x='Z';
+		int q=0b1010_110;
+		int r=0x1101;
+		System.out.print(a +" is INt \n"+ b +" is Long \n"+ c +" is Float \n"+ d +" is also Float \n"+ e +" is Double\n"+ x +" is Char \n"+ q +" is for Binary \n"+ r +" is for Hexadecimal \n");
+	}
+}
