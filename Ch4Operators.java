2e3822a55f4eff7c5c899a0d470c0e057153fa08
ChZainali27 <104452690+ChZainali27@users.noreply.github.com>
2022-05-13 23:12:31 +0500

    Add files via upload

---
+++ Ch4Operators.java
@@ -0,0 +1,32 @@
+public class Ch4Operators
+{
+	public static void main (String arg[])
+	{
+	    /*	int a,b,c,d,e;
+		a=1+1;
+		b=a*3;
+		c=b-2;
+		d=c/4;
+		System.out.print("a=" + a + "\t b=" + b + 
+		"\t c=" + c + "\t d=" + d);
+	     
+
+
+		double a,b,c,d,e;
+		a=1+1;
+		b=a*3;
+		c=b-2;
+		d=c/4;
+		System.out.print("a=" + a + "\t b=" + b + 
+		"\t c=" + c + "\t d=" + d);
+	    */
+
+		
+
+		int a=76;
+		float b=43f;
+		System.out.print("now a=" + a%15 + "\n now b=" +b%15);
+
+
+	}
+}
