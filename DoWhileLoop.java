cfabfe961ec9f31639745cb6bb755fb9e8f6d0b3
ChZainali27 <104452690+ChZainali27@users.noreply.github.com>
2022-05-14 23:38:13 +0500

    Add files via upload

---
+++ DoWhileLoop.java
@@ -0,0 +1,68 @@
+public class DoWhileLoop
+{
+	public static void main (String arg[])
+	{
+	   /*	System.out.print("hello");
+		int a=1;
+		while(a<=6)
+		{
+			System.out.print("\n a=" + a*2);
+			a++;
+		}   */	
+/*
+hello
+a=2
+a=4
+a=6
+a=8
+a=10
+a=12
+*/
+
+
+	/*	int a=1;
+		while(a<=5)
+		{
+			int b=a;
+			while(b<=5)
+			{
+				System.out.print("o");
+				b++;
+			}
+			a++;
+			System.out.print("\n");		
+		}     */
+
+/*
+oooo
+ooo
+oo
+o
+*/
+
+
+	/*	int a=1;
+		while(a<=5)
+		{
+			int b=1;
+			while(b<=4)
+			{
+				System.out.print(b);
+				b++;
+			}
+			a++;
+			System.out.print("\n");
+		}   */
+
+/*
+1234
+1234
+1234
+1234
+1234
+*/
+
+
+
+	}
+}
