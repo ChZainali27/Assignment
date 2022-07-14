390999b713c31410b6ab1ee6a3b8d357fc9eacad
ChZainali27 <104452690+ChZainali27@users.noreply.github.com>
2022-07-14 23:36:48 +0500

    Add files via upload

---
+++ Encapsulation.java
@@ -0,0 +1,14 @@
+class Arrayy
+{
+	public static void main (String zoro[])
+	{
+		int [] m=new int[6]
+		int b=0;
+		for (int i=0; i<6; i++)
+		{
+			m[i]=b;
+			b=b+10;
+			System.out.println(m[i]);
+		}
+	}
+}
