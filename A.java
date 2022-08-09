67fa01701c0087a5608c58dc7d234da14e51d48c
ChZainali27 <104452690+ChZainali27@users.noreply.github.com>
2022-08-09 18:24:29 +0500

    Add files via upload

---
+++ A.java
@@ -0,0 +1,42 @@
+package p1;
+
+/****
+public class A
+{
+	public int show()
+	{
+		int c;
+		int a,b;
+		a=10;
+		b=20;
+		//System.out.println("A+B = "+ c);
+		return c=a+b;
+	}
+	public void msg()
+	{
+		System.out.println("I--am--ZORO____240");
+	}
+	// CANNOT ADD MAIN METHOD IN THIS PACKAGE CLASS
+	//public static void main (String zoro[])
+	//{
+	//	A a=new A();
+	//	int x=a.show(5,5);
+	//	//System.out.println(x);
+	//	//o.msg();
+	//}
+}
+**/
+public class A
+{
+	public int array()
+	{
+		int [] a=new int [5];
+		a[0]=2;
+		a[1]=4;
+		a[2]=6;
+		a[3]=8;
+		a[4]=10;
+		return a[3];
+	}
+}
+
